41ed00f71c87607e6e1d8473d45f1d297a31adc6
beka.brother <aka.beka94@mail.ru>
2020-10-16 13:12:26 +0600

    WebProject tables

--- Database/Tables/WebProject/Inventory_Tasks.sql
+++ Database/Tables/WebProject/Inventory_Tasks.sql
@@ -1,4 +1,18 @@
 ﻿CREATE TABLE [dbo].[Inventory_Tasks]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] [nvarchar](50) NULL,
+	[DocNum] [nvarchar](50) NULL,
+	[OrderDate] [datetime] NULL,
+	[LocationFrom] [nvarchar](50) NULL,
+	[LocationFromId] [int] NULL,
+	[LocationTo] [nvarchar](50) NULL,
+	[LocationToId] [int] NULL,
+	[DivisionaName] [nvarchar](50) NULL,
+	[Quantity] [numeric](18, 0) NULL,
+	[Volume] [numeric](18, 4) NULL,
+	[OperType] [uniqueidentifier] NULL,
+	[LocationTO1c] [nvarchar](50) NULL,
+	[LocationTO1cc] [binary](50) NULL,
+	[ROT] [nvarchar](50) NULL,
+	[PlanNum] [nvarchar](50) NULL
 )
