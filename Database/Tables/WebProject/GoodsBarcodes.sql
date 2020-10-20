ccfb490836d3a444c85e80c36c24f3c17c653eef
beka.brother <aka.beka94@mail.ru>
2020-10-20 11:52:18 +0600

    GoodsBarcodesDropFK

--- Database/Tables/WebProject/GoodsBarcodes.sql
+++ Database/Tables/WebProject/GoodsBarcodes.sql
@@ -2,6 +2,4 @@
 (
 	[GoodId] [int] NOT NULL,
 	[BarcodeId] [int] NOT NULL, 
-    CONSTRAINT [FK_GoodsBarcodes_Goods_GoodId] FOREIGN KEY ([GoodId]) REFERENCES [Goods]([Id]), 
-    CONSTRAINT [FK_GoodsBarcodes_BarCodes_BarcodeId] FOREIGN KEY ([BarcodeId]) REFERENCES [BarCodes]([Id])
 )
---
+++ DatabaseMigrations/Migrations/202010201133_GoodsBarcodesDropFK.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(202010201133)]
+    public class GoodsBarcodesDropFK : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            Execute.Sql(@"GO
+ALTER TABLE [dbo].[GoodsBarcodes] DROP CONSTRAINT [FK_GoodsBarcodes_BarCodes_BarcodeId];
+ALTER TABLE [dbo].[GoodsBarcodes] DROP CONSTRAINT [FK_GoodsBarcodes_Goods_GoodId];");
+        }
+    }
+}
