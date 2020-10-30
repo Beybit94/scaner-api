d28b975c2f47912153372834edf750c108c07551
Beybit94 <aka.beka94@mail.ru>
2020-10-30 10:55:55 +0600

    Orderby Barcode

--- Data/Repositories/GoodRepository.cs
+++ Data/Repositories/GoodRepository.cs
@@ -116,7 +116,7 @@ FROM GOODS G
 JOIN GOODSBARCODES GB (NOLOCK) ON GB.GOODID = G.GOODID
 JOIN  BARCODES BC (NOLOCK) ON BC.BARCODEID = GB.BARCODEID	
 WHERE G.GOODARTICLE LIKE @GoodArticle
-GROUP BY G.GOODID,G.GOODARTICLE, G.GOODNAME", new { @GoodArticle = "%"+_query.GoodArticle });
+GROUP BY G.GOODID,G.GOODARTICLE, G.GOODNAME, BC.BARCODE", new { @GoodArticle = "%"+_query.GoodArticle });
     return entity.ToList();
         }
 
