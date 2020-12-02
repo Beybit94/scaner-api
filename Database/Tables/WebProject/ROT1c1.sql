c032dabc085fe0e1631163aed589606fd19b4049
beka.brother <aka.beka94@mail.ru>
2020-12-02 11:54:56 +0600

    Revert "Trigger_Scaner_Goods"
    This reverts commit 78b72fe1d46ab5a696dbf2e3537d12517e88578a.

---
+++ Database/Tables/WebProject/ROT1c1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ROT1c1]
+(
+	[_IDRRef] [binary](16) NULL,
+	[_Date_Time] [datetime] NULL,
+	[_NumberPrefix] [datetime] NULL,
+	[_Number] [nvarchar](50) NULL,
+	[GoodMoveDocumentIDRRef] [binary](16) NULL,
+	[DivisionIDRRef] [binary](16) NULL,
+	[LocationIDRRef] [binary](16) NULL,
+	[Volume] [numeric](12, 4) NULL,
+	[PlanNum] [nvarchar](50) NULL, 
+) ON [PRIMARY]
