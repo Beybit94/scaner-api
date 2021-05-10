8ae7738f3e6df1bbad2f8946c09e18dedb4d9935
Beybit Angsabay <aka.beka94@mail.ru>
2021-05-10 12:56:41 +0600

    Users index by UserName

---
+++ DatabaseMigrations/Migrations/202105101201_UsersIndex.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(202105101201)]
+    public class UsersIndex : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            Execute.Sql(@"
+GO
+CREATE NONCLUSTERED INDEX [IX_Users_UserName]
+    ON [dbo].[Users]([UserName] ASC);");
+        }
+    }
+}
