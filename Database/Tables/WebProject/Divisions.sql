1d682bc41596ff6df323bdcef463d59c37b34fcf
Beybit Angsabay <aka.beka94@mail.ru>
2021-06-18 11:54:05 +0600

    Location/Division

---
+++ Database/Tables/WebProject/Divisions.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Divisions] (
+    [Id]               INT              IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
+    [Name]             NVARCHAR (250)   NOT NULL,
+    [Guid]             UNIQUEIDENTIFIER NOT NULL,
+    [ParentId]         INT              NULL,
+    [IsDeleted]        BIT              CONSTRAINT [DF_Divisions_IsDeleted] DEFAULT ((0)) NOT NULL,
+    [Code]             NVARCHAR (10)    NULL,
+    [GeoX]             NVARCHAR (100)   NULL,
+    [GeoY]             NVARCHAR (100)   NULL,
+    [IDRRef]           BINARY (16)      NULL,
+    [PriceTypeIDRRef]  BINARY (16)      NULL,
+    [CategoryRatingId] INT              NULL,
+    CONSTRAINT [PK_Divisions] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Divisions_Divisions] FOREIGN KEY ([ParentId]) REFERENCES [dbo].[Divisions] ([Id])
+);
---
+++ Database/Tables/WebProject/Locations.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Locations] (
+    [LocationId]     INT              IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
+    [LocationCode]   NVARCHAR (10)    NULL,
+    [LocationName]   NVARCHAR (250)   NULL,
+    [LocationPrefix] NVARCHAR (4)     NULL,
+    [LocationGuid]   UNIQUEIDENTIFIER NULL,
+    [DivisionId]     INT              NULL,
+    [IDRRef]         BINARY (16)      NULL,
+    [IsDeleted]      INT              NULL,
+    CONSTRAINT [PK_Locations] PRIMARY KEY CLUSTERED ([LocationId] ASC)
+);
